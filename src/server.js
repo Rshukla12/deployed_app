8a25e45f4250a41d92559d8747307b2cf5a6e506
Ravi Shukla <rshuklabbb@gmail.com>
2022-02-25 18:20:05 +0530

    Added env for port

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const connect = require("./config/db");
 const userRouter = require("./routes/user.routes"); 
 const tweetRouter = require("./routes/tweet.route");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
